6571662ff8f3f8d4eaa70451d3a31d4b944938c7
Dan Taylor <dan.taylor@crawlorsoft.com>
2016-03-17 17:49:12 -0400

    Fixing the documentation for the cs-notify module and cs-notifications directive to link the directive documentation into the cs-notify module.
    File:  cs-notify-module.js
    Modifications:
    
    1. Add module overview documentation.
    
    File cs-notify.directive.js
    Modifications:
    
    1. Update to indicate cs-notifications is part of the cs-notify module.

--- src/cs-notify.directive.js
+++ src/cs-notify.directive.js
@@ -1,17 +1,18 @@
-/**
- * @ngdoc directive
- * @name cs-notifications
- * @description
- * Creates a new cs-notifications area which will display the newest notification which has been received by the system.
- * This directive makes use of the CSNotificationsController in order to gather events and notifications for display
- * to the user.
- *
- * @restrict E
- * @scope
- * @param {number} [ellipsisLength] The length the string may be before it becomes shortened and has '...' appended.
- * @param {Object}  iconSet An Object containing the classPrefix and [(error)(warning)(success)(info)]Icon values.
- */
-angular.module('cs-notify').directive('csNotifications', [
+angular.module('cs-notify')
+    /**
+     * @ngdoc directive
+     * @name cs-notify.directive:cs-notifications
+     * @description
+     * Creates a new cs-notifications area which will display the newest notification which has been received by the system.
+     * This directive makes use of the CSNotificationsController in order to gather events and notifications for display
+     * to the user.
+     *
+     * @restrict E
+     * @scope
+     * @param {number} [ellipsisLength] The length the string may be before it becomes shortened and has '...' appended.
+     * @param {Object}  iconSet An Object containing the classPrefix and [(error)(warning)(success)(info)]Icon values.
+     */
+    .directive('csNotifications', [
     '$log',
     function($log) {
         $log.debug('Creating Directive...');
--- src/cs-notify.module.js
+++ src/cs-notify.module.js
@@ -1,1 +1,37 @@
+/**
+ * @ngdoc overview
+ * @name cs-notify
+ * @module cs-notify
+ * @description
+ *
+ * # Installation
+ *
+ * First, include `cs-notify.js` in your HTML:
+ * ```html
+ * <script src="angular.js"></script>
+ * <script src="cs-notify.js"></script>
+ * ```
+ *
+ * CS-Notify can be installed from one of the following locations:
+ *
+ * * [Bower](http://bower.io)
+ *      `__Will be documented once the first release is completed.__`
+ * * [NPM](http://npmjs.com)
+ *      `__Will be documented once the first release is completed.__`
+ *
+ * After installation, the module can be loaded into your application by indicating it is a dependent module:
+ * ```js
+ * angular.module('app', ['cs-notify']);
+ * ```
+ *
+ * The CS-Notification directive and service are now ready to be used to help prevent annoying pop-ups and popovers
+ * from hiding application content while still informing users of important status updates.
+ *
+ * # Usage
+ *
+ * The cs-notifications directive allows the specification of an icon-set to prepend to incoming notifications without
+ * tying the user down to any one particular font or icon family.  The only restriction is that the icon set must
+ * currently be an icon font such as [Bootstrap Glyphicons](http://getbootstrap.com/components/#glyphicons) or
+ * [Font Awesome](http://fontawesome.io).
+ */
 angular.module('cs-notify', []);
