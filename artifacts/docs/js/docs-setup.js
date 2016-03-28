7b33b535444c0cb64377c3af4067997a624dc617
Dan Taylor <dan.taylor@crawlorsoft.com>
2016-03-28 19:13:01 -0400

    Adding artifacts for ghpages documentation

---
+++ artifacts/docs/js/docs-setup.js
@@ -0,0 +1,83 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "cs-notify",
+      "shortName": "cs-notify",
+      "type": "overview",
+      "moduleName": "cs-notify",
+      "shortDescription": "Installation",
+      "keywords": "allows angular annoying api app application awesome bower content cs-notification cs-notifications cs-notify currently dependent detailed directive documentation family font glyphicons help hiding html icon icon-set include incoming indicating informing install installation installed io js loaded locations module notifications npm overview pop-ups popovers prepend prevent ready restriction service set specific specification src status tying updates usage user users"
+    },
+    {
+      "section": "api",
+      "id": "cs-notify.directive:cs-notifications",
+      "shortName": "cs-notifications",
+      "type": "directive",
+      "moduleName": "cs-notify",
+      "shortDescription": "Creates a new cs-notifications area which will display the newest notification which has been received by the",
+      "keywords": "$scope advanced angular api appended application area attribute awesome basic bootstrapglyphiconsiconset bootstraps classprefix code constantly controller creates cs-notification-service cs-notifications cs-notifications-service cs-notify csnotificationscontroller csns custom customiconset data data-icon-set data-ng-app data-ng-controller default defined definitions desired directive directives display error erroricon events fa fa-check fa-exclamation fa-fw fa-info-circle fa-times font fontawesomeiconset footer function gather glyphicons icon iconset infoicon json length main module myapp newest notification notifications object order passed passing prefix provided received remain sc scope set sets setting shortened somecontroller sticky string success successicon suggested system tag usage user values variable views visible warning warningicon web"
+    },
+    {
+      "section": "api",
+      "id": "cs-notify.object:CS_NOTIFICATION_TYPE",
+      "shortName": "CS_NOTIFICATION_TYPE",
+      "type": "object",
+      "moduleName": "cs-notify",
+      "shortDescription": "A basic enumeration object containing the different notifications which can be sent to the cs-notifications",
+      "keywords": "api basic cs-notifications cs-notify cs_notification_type csnotificationservice directive enumeration error iconless indicates info notification notifications object success warning"
+    },
+    {
+      "section": "api",
+      "id": "cs-notify.object:CSNotification",
+      "shortName": "CSNotification",
+      "type": "object",
+      "moduleName": "cs-notify",
+      "keywords": "api class controllers cs-notify csnotification data ellipsised full getfullmessage getshortmessage info marked message method module non-ellipsised non-shortened notification object passing responsible services short success true type warning"
+    },
+    {
+      "section": "api",
+      "id": "cs-notify.service:csNotificationService",
+      "shortName": "csNotificationService",
+      "type": "service",
+      "moduleName": "cs-notify",
+      "shortDescription": "The csNotificationService provides:",
+      "keywords": "$log $rootscope __ellipsispoint _maxnotifications_ _x_ ability accomplish adjust adjusted adjusts api appended appends area array assume asterisk awesome basic beer bolt bootstrap bootstrapglyphiconiconset bring call calling case chance change changenotificationmaximum characters chosen circle class classprefix clear clearnotifications clears copy create created createiconsetdefinition creates creation cs-notifications cs-notify cs-notify-new-notification csnotification csnotificationservice css current custom customfontawesomeset customized data default default_ellipsis_pt definition definitions depending directive discarded display displayed drinks ellipsis ellipsislength ellipsispoint emit emits emitted empty emptyiconset equal equivalent error erroricon errors event events examples excess fa fa-asterisk fa-beer fa-bolt fa-check-circle fa-exclamation-circle fa-exclamation-triangle fa-fw fa-info-circle fa-stop-circle fixed font font-awesome fontawesomeiconset freeze frozen full function future generated getnotifications getshortmessage glyphicon glyphicon-ban-circle glyphicon-info-sign glyphicon-ok-circle glyphicon-warning-sign glyphicons greater held hello helper higher icon iconless icons increase increased infinity info infoicon inside io length lightning limit list listeners log lost max maximum maxnotifications memory message messages method methods msg non-numerical notification notifications notificationtype number object objects occur oldest order output passed permanently permitted point prefix prefixed previous property provided queue registered removed reported required retain retained retrieval retrieve returned returns rootscope rotated send senderrornotification sendiconlessnotification sendinfonotification sendnotification sendsuccessnotification sendwarningnotification service set setellipsislength sets short shortened solid specific status string strings success successicon symbol ten threshold total trimmed type undefined usage values var vcr warning warningicon width"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "__file": "_FAKE_DEST_/js/docs-setup.js",
+  "__options": {
+    "startPage": "/api",
+    "scripts": [
+      "js/angular.min.js",
+      "js/angular-animate.min.js",
+      "js/marked.js"
+    ],
+    "styles": [],
+    "title": "API Documentation",
+    "html5Mode": false,
+    "editExample": true,
+    "navTemplate": false,
+    "navContent": "",
+    "navTemplateData": {},
+    "loadDefaults": {
+      "angular": true,
+      "angularAnimate": true,
+      "marked": true
+    }
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "js/angular.min.js",
+    "js/angular-animate.min.js",
+    "js/marked.js"
+  ]
+};
